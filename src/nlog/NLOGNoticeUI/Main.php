b0d5230828556f3d1f9257a3e00b78210273f572
NLOG <sorisem4106@naver.com>
2017-11-26 10:50:34 +0900

    release 1.0

---
+++ src/nlog/NLOGNoticeUI/Main.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace nlog\NLOGNoticeUI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
+
+class Main extends PluginBase implements Listener {
+	
+	public $player;
+	
+	/** @var Settings */
+	public $setting;
+	
+	public $time;
+	
+	const FORM_ID = 1412;
+	
+	const TAG = "§b§o[ NoticeUI ] §7";
+	
+	public function onEnable() {
+		
+		@mkdir($this->getDataFolder());
+		$this->setting = new Settings($this->getDataFolder() . "setting.yml");
+		
+		$this->player = new Config($this->getDataFolder() . "players.yml", Config::YAML);
+		
+		$this->time = new Config($this->getDataFolder() . "time.yml", Config::YAML);
+		if (!$this->time->get("time") || $this->time->get("time") !== $this->getDay()) {
+			$this->time->set("time", $this->getDay());
+			$this->time->save();
+			
+			$this->player->setAll([]);
+			$this->player->save();
+		}
+		
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info("NoticeUI 플러그인 활성화");
+	}
+	
+	public function onDataRecieve(DataPacketReceiveEvent $ev) {
+		$pl = $ev->getPlayer()->getName();
+		$pk = $ev->getPacket();
+		if ($pk instanceof ModalFormResponsePacket && $pk->formId === self::FORM_ID) {
+			$data = json_decode($pk->formData, true);
+			if ($data) {
+				$this->player->set($ev->getPlayer()->getName(), true);
+				$this->player->save();
+			}
+		}
+	}
+	
+	public function getDay() {
+		return date("Y-m-d");
+	}
+	
+	public function onPlayerJoin (PlayerJoinEvent $ev) {
+		$name = $ev->getPlayer()->getName();
+		if ($this->player->get($name) === false) {
+			sleep(5);
+			$json = [];
+			$json["type"] = "modal";
+			$json["title"] = $this->setting->getTitle($name);
+			$json["content"] = $this->setting->getMessage($name);
+			$json["button1"] = ">> 하루 동안 보지 않기 <<"; //true
+			$json["button2"] = ">> 닫기 <<"; //false
+			
+			$pk = new ModalFormRequestPacket();
+			$pk->formId = self::FORM_ID;
+			$pk->formData = json_encode($json);
+			
+			$ev->getPlayer()->dataPacket($pk);
+		}
+	}
+	
+	
+} //클래스 괄호
+
+?>
---
+++ src/nlog/NLOGNoticeUI/Settings.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace nlog\NLOGNoticeUI;
+
+use pocketmine\utils\Config;
+use pocketmine\Server;
+use pocketmine\Player;
+use onebone\economyapi\EconomyAPI;
+
+class Settings {
+	
+	/** @var Config */
+	protected $config;
+	
+	/** @var Server */
+	protected $server;
+	
+	/** @var array */
+	protected $availableParameter;
+	
+	public function __construct(string $path) {
+		$this->config = new Config($path, Config::YAML);
+		$this->server = Server::getInstance();
+		$this->availableParameter = [
+				"@playername", "@playercount", "@playermaxcount", "@motd", "@mymoney", "@health", "@maxhealth", "@year", "@month", "@day", "@hour"
+		];
+		
+		$this->init();
+	}
+	
+	protected function init() {
+		if (!$this->config->get("title") || is_array($this->config->get("title"))) {
+			$this->config->set("title", "서버에 오신 것을 환영합니다.");
+			$this->save();
+		}
+		if (!$this->config->get("message") || is_array($this->config->get("message"))) {
+			$this->config->set("message", "안녕하세요 @playername님\n서버에 오신 것을 환영합니다!");
+			$this->save();
+		}
+	}
+	
+	public final function getConfig(): Config {
+		return $this->config;
+	}
+	
+	public final function save(bool $async = false) {
+		$this->config->save($async);
+	}
+	
+	public function getMessage(Player $player, $economy = null) {
+		$economy instanceof EconomyAPI ? $economy : EconomyAPI::getInstance();
+		$msg = $this->config->get("message");
+		$msg = str_replace($this->availableParameter, [
+				$player->getName(), 
+				count($this->server->getOnlinePlayers()), 
+				$this->server->getMaxPlayers(),
+				$this->server->getNetwork()->getName(),
+				$economy->myMoney($player),
+				$player->getHealth(),
+				$player->getMaxHealth(),
+				date("Y"),
+				date("m"),
+				date("d"),
+				date("g")
+		], $msg);
+		
+		$msg = str_replace('\n', "\n", $msg);
+		
+		return $msg;
+	}
+	
+	public function getTitle(Player $player, $economy = null) {
+		$economy instanceof EconomyAPI ? $economy : EconomyAPI::getInstance();
+		$msg = $this->config->get("title");
+		$msg = str_replace($this->availableParameter, [
+				$player->getName(), 
+				count($this->server->getOnlinePlayers()), 
+				$this->server->getMaxPlayers(),
+				$this->server->getNetwork()->getName(),
+				$economy->myMoney($player),
+				$player->getHealth(),
+				$player->getMaxHealth(),
+				date("Y"),
+				date("m"),
+				date("d"),
+				date("g")
+		], $msg);
+		
+		$msg = str_replace('\n', "\n", $msg);
+		
+		return $msg;
+	}
+}
